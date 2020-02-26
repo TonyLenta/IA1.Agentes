4b27e065b5f670ee92293cd94dc7dc9c7a544a8d
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-02-26 13:40:52 -0500

    Creacion de scheduler, agentes varios

---
+++ src/Tarea/AppMain.java
@@ -0,0 +1,54 @@
+package Examen.ExamenTarea;
+
+/**
+ *
+ * @author tony_
+ */
+ 
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.*;
+
+public class AppMain 
+{
+    public static void main(String args[]) throws StaleProxyException 
+    {          
+        Runtime rt = Runtime.instance();
+	rt.setCloseVM(true);
+	// Create a default profile
+	Profile profile = new ProfileImpl("localhost", 1069, "main");
+	AgentContainer mainContainer = jade.core.Runtime.instance().createMainContainer(profile);
+        AgentController rma1 = null;
+        AgentController rma2 = null;
+        AgentController rma3 = null;
+        AgentController rma4 = null;
+        AgentController rma5 = null;
+        
+	try 
+        {
+            //Agente humedad   
+            rma2 = mainContainer.createNewAgent("humedad", "Examen.ExamenTarea.AgenteHumedad", new Object[0]);
+            rma2.start();   
+            
+            // Agente temperatura 1
+            rma4 = mainContainer.createNewAgent("temperatura", "Examen.ExamenTarea.AgenteTemperatura1", new Object[0]);
+            rma4.start();
+          
+            //Agente temperatura 2
+            rma5 = mainContainer.createNewAgent("temperatura2", "Examen.ExamenTarea.AgenteTemperatura2", new Object[0]);
+            rma5.start();  
+           
+            //Agente Compresor
+            rma1 = mainContainer.createNewAgent("compresor", "Examen.ExamenTarea.AgenteVelocidadCompresor", new Object[0]);
+            rma1.start();
+            //Agente Fucion temperatura
+            rma3 = mainContainer.createNewAgent("fusion", "Examen.ExamenTarea.AgenteFusion", new Object[0]);
+            rma3.start();
+        } 
+        catch (StaleProxyException e) 
+        {
+            e.printStackTrace();
+	}
+     }
+}
